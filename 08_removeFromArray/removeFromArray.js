c513f119474d30bb98029fa97d1b4dfdaf3876db
skdoosh <149157430+skdoosh@users.noreply.github.com>
2025-05-28 18:25:57 +0530

    8 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    return array.filter(val=>(!values.includes(val)));
 };
+// const removeFromArray = function(array, ...args) {
+//   return array.filter(val => !args.includes(val))
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
