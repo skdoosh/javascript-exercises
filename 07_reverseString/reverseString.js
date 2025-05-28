6fbabff78d7155f9b0b4150d0bec8c46d8ad37c0
skdoosh <149157430+skdoosh@users.noreply.github.com>
2025-05-28 18:01:40 +0530

    7 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arr = str.split('');
+    let b = arr.reverse();
+    let reversed = b.join('');
+    return reversed;
 
 };
-
+console.log(reverseString('hello'))
 // Do not edit below this line
 module.exports = reverseString;
