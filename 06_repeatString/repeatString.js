3a9f91f92d9ead8682001cb3d6074eae3136b504
skdoosh <149157430+skdoosh@users.noreply.github.com>
2025-05-28 17:53:46 +0530

    6 down

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(foo, bar) {
+    if(bar<0) return('ERROR');
+    let result = '';
+    for(let i = 0; i<bar; i++){
+        result+=foo;
+    }
+    return result;
 };
 
 // Do not edit below this line
