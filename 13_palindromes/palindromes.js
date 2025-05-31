c3d66940985242f5b5006aeb6e22ee564f7b5258
skdoosh <149157430+skdoosh@users.noreply.github.com>
2025-05-31 16:44:20 +0530

    13 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    return str.toLowerCase().replace(/[^a-z0-9]/g,"") === str.toLowerCase().replace(/[^a-z0-9]/g,"").split('').reverse().join('');
 };
-
+let str = 'r3ace3car';
+console.log(str.toLowerCase().replace(/[^a-z0-9]/g,""));
+console.log(str.toLowerCase().replace(/[^a-z]/g,"").split('').reverse().join(''))
+// palindromes('ZZZZ car, a man, a maracaz.');
 // Do not edit below this line
 module.exports = palindromes;
