40949490e993231d15a43ef99aa2e336f8c37612
skdoosh <149157430+skdoosh@users.noreply.github.com>
2025-06-01 16:15:51 +0530

    16 done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((acc, curr)=>{
+        if(acc === ({})) return curr;
+        let ad = acc.yearOfDeath || new Date().getFullYear();
+        let cd = curr.yearOfDeath || new Date().getFullYear();
 
+        if ((ad-acc.yearOfBirth)>(cd-curr.yearOfBirth)){
+            return acc;
+        }else return curr;
+    },{})
 };
 
 // Do not edit below this line
